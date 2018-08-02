905c78000984473a050fa7c545bae2c094ee100f
jinkun2014 <jinkun2014@163.com>
2018-08-02 17:15:18 +0800

    对mysql优化

--- src/main/java/me/jinkun/db/mysql/MainMysql.java
+++ src/main/java/me/jinkun/db/mysql/MainMysql.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class MainMysql {
     public static void main(String[] args) throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/rds_sys", "root", "root");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.20.126:3306/rap", "root", "123456");
 
         List<Map> tableList = getTableList(conn);
 
@@ -42,14 +42,14 @@ public class MainMysql {
             String TABLE_NAME = rs.getString("NAME");
             String COMMENTS = rs.getString("COMMENT");
             map.put("TABLE_NAME", TABLE_NAME);
-            map.put("COMMENTS", COMMENTS == null ? "" : COMMENTS);
+            map.put("COMMENTS", COMMENTS == null ? "" : COMMENTS.replaceAll("\r|\n\t", "").trim());
 
             //获取列
             List<Map> columnList = getColumnList(conn, TABLE_NAME);
             map.put("COLUMNS", columnList);
 
             //if (TABLE_NAME.startsWith("ZJ"))
-                tableList.add(map);
+            tableList.add(map);
             System.out.println("TABLE_NAME ==>" + TABLE_NAME + "  COMMENTS==>" + COMMENTS);
         }
         rs.close();
@@ -65,7 +65,7 @@ public class MainMysql {
 
         //SHOW FULL COLUMNS FROM sys_org
         String sql =
-                "SHOW FULL COLUMNS FROM "+ tableName;
+                "SHOW FULL COLUMNS FROM " + tableName;
 
         PreparedStatement ps = conn.prepareStatement(sql);
         //ps.setString(1, tableName);
@@ -73,24 +73,28 @@ public class MainMysql {
         while (rs.next()) {
             Map map = new HashMap<>();
 
-            String COLUMN_NAME = rs.getString("FIELD");
-            String DATA_TYPE = rs.getString("TYPE");//varchar(200)
+            String COLUMN_NAME = rs.getString("FIELD").toUpperCase();
+            String DATA_TYPE = rs.getString("TYPE").toUpperCase();//varchar(200)
             String DATA_LENGTH = "";//rs.getString("DATA_LENGTH");
+            if (DATA_TYPE.indexOf("(") != -1) {
+                DATA_LENGTH = DATA_TYPE.substring(DATA_TYPE.indexOf("(")+1, DATA_TYPE.indexOf(")"));
+                DATA_TYPE = DATA_TYPE.substring(0, DATA_TYPE.indexOf("("));
+            }
             String DATA_DEFAULT = rs.getString("DEFAULT");
             String NULLABLE = rs.getString("NULL");
             String COMMENTS = rs.getString("COMMENT");
             String PRIMARY_KEY = rs.getString("KEY");
 
-            map.put("COLUMN_NAME", COLUMN_NAME.toUpperCase());
-            map.put("DATA_TYPE", DATA_TYPE.toUpperCase());
+            map.put("COLUMN_NAME", COLUMN_NAME);
+            map.put("DATA_TYPE", DATA_TYPE);
             map.put("DATA_LENGTH", DATA_LENGTH);
             map.put("DATA_DEFAULT", DATA_DEFAULT == null ? "" : DATA_DEFAULT);
             map.put("NULLABLE", "NO".equals(NULLABLE) ? false : true);
             map.put("COMMENTS", COMMENTS == null ? "" : COMMENTS);
-            map.put("PRIMARY_KEY", "PRI".equals(PRIMARY_KEY) ? true: false);
+            map.put("PRIMARY_KEY", "PRI".equals(PRIMARY_KEY) ? true : false);
             columnList.add(map);
 
-            System.out.println("COLUMN_NAME ==>" + COLUMN_NAME + "  DATA_TYPE==>" + DATA_TYPE + "  DATA_LENGTH==>" + DATA_LENGTH +" NULLABLE==>"+NULLABLE +"  COMMENTS==>" + COMMENTS + " PRIMARY_KEY==>" + PRIMARY_KEY);
+            System.out.println("COLUMN_NAME ==>" + COLUMN_NAME + "  DATA_TYPE==>" + DATA_TYPE + "  DATA_LENGTH==>" + DATA_LENGTH + " NULLABLE==>" + NULLABLE + "  COMMENTS==>" + COMMENTS + " PRIMARY_KEY==>" + PRIMARY_KEY);
         }
         rs.close();
         ps.close();
